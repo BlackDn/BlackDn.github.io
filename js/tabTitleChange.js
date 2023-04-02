4ccf356b1fdfdbfaba13786a1c4ae9fb7b53f00d
blackdn <940045828@qq.com>
2023-04-02 18:39:07 +0800

    [blackdn] feat: replace visibilityState with document.hidden

--- js/tabTitleChange.js
+++ js/tabTitleChange.js
@@ -1,7 +1,7 @@
 
 // change tab title
 document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === 'hidden') {
+    if (document.hidden) {
         document.title = '(づ￣ ³￣)づ人家在这等你哦'
     } else {
         document.title = 'WelcomeQwQ'
