6ec4384b7f784b6baf6e649ce0242cf4f00e641f
blackdn <940045828@qq.com>
2024-02-07 11:53:05 +0800

    refactor: check style

--- js/tabTitleChange.js
+++ js/tabTitleChange.js
@@ -1,9 +1,10 @@
-
 // change tab title
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        document.title = '(づ￣ ³￣)づ人家在这等你哦'
-    } else {
-        document.title = 'WelcomeQwQ'
-    }
-})
+document.addEventListener("visibilitychange", function () {
+  let originalTitle = "";
+  if (document.hidden) {
+    originalTitle = document.title;
+    document.title = "(づ￣ ³￣)づ人家在这等你哦";
+  } else {
+    document.title = "WelcomeQwQ";
+  }
+});
