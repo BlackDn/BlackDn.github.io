907c783378ee29add27991027c7169642205970c
BlackDn <940045828@qq.com>
2025-03-13 18:33:08 +0800

    feat: change tab title when main page hidden

--- js/tab-title-change.js
+++ js/tab-title-change.js
@@ -1,8 +1,6 @@
 // change tab title
 document.addEventListener("visibilitychange", function () {
-  let originalTitle = "";
   if (document.hidden) {
-    originalTitle = document.title;
     document.title = "(づ￣ ³￣)づ人家在这等你哦";
   } else {
     document.title = "WelcomeQwQ";
